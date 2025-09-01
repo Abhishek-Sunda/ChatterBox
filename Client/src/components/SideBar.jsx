956459bb65a9b5657e93d8a731a1e73361d1a13e
AbhishekSunda1998 <abhisheksunda@gmail.com>
2025-09-01 06:08:04 +0530

    Basic Sidebar Design Completed

--- Client/src/components/SideBar.jsx
+++ Client/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import {useNavigate} from 'react-router-dom'
 
 const SideBar = ({SelectedUser, setSelectedUser}) => {
@@ -19,14 +19,33 @@ const SideBar = ({SelectedUser, setSelectedUser}) => {
                 </div>
             </div>
         </div>
-
+        {/* Search Bar */}
         <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4'>
             <img src={assets.search_icon} alt="search" className='w-3' />
-            <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Select User...'/>
+            <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search For User...'/>
+        </div>
+
+        <div className='flex flex-col'>
+            {userDummyData.map((user, index) => 
+                (
+                    <div onClick={()=>{setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${SelectedUser?._id === user.id && 'bg-[#282142]/50'} `}>
+                        <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
+                        <div className='flex flex-col leading-5'>
+                            <p>{user.fullName}</p>
+                            {
+                                index < 3 
+                                ? <span className='text-green-400 text-xs'>Online</span> 
+                                : <span className='text-neutral-400-400 text-xs'>Offline</span>
+                            }
+                        </div>
+                        {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50 '>{index}</p>}
+                    </div>
+                ))
+            }
         </div>
 
     </div>
   )
 }
 
-export default SideBar
+export default SideBar  
