4a8a436ca78fc41c1d07a938b09439a34c6928b3
AbhishekSunda1998 <abhisheksunda@gmail.com>
2025-10-05 07:36:49 +0530

    FrontEnd Is Done

--- Client/src/pages/ProfilePage.jsx
+++ Client/src/pages/ProfilePage.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import assets from '../assets/assets';
 
 const ProfilePage = () => {
+
+  const [SelectedImg, SetSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name, SetName] = useState("Anish Fageria");
+  const [bio, SetBio] = useState("Hi, I am pokars");
+  const handleSubmit = async(e)=>
+  {
+    e.preventDefault();
+    navigate('/');
+  }
   return (
-    <div><h1>Profile Page</h1></div>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center '>
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg ">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1 ">
+          <h3 className="text-lg">Profile Details</h3>
+          <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer '>
+            <input onChange={(e)=>SetSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={SelectedImg ? URL.createObjectURL(SelectedImg) : assets.avatar_icon} className={ ` w-12 h-12 ${SelectedImg} && rounded-full`} />
+            Upload Profile Image
+          </label>
+          <input onChange={(e)=>SetName(e.target.value)} value={name} type='text' required placeholder='Your Name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'/>
+
+          <textarea onChange={(e)=>SetBio(e.target.value)} value={bio} placeholder='Write Bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2  focus:ring_violet-500' row={4}></textarea>
+
+          <button type='Submit' className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer "> Update Profile</button>
+        </form>
+        <img className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" src={ assets.logo_icon}alt='' />
+      </div>
+    </div>
   )
 }
 
